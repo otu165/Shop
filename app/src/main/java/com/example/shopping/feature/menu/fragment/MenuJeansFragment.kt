382f182d5138a6f581172a38619b1f455b038bec
otu165 <micheu35@gmail.com>
2020-10-24 21:48:52 +0900

    juyeon_MenuActivity_add more fragment

---
+++ app/src/main/java/com/example/shopping/feature/menu/fragment/MenuJeansFragment.kt
@@ -0,0 +1,23 @@
+package com.example.shopping.feature.menu.fragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shopping.R
+
+class MenuJeansFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        Log.d(TAG, "${TAG} created")
+        return inflater.inflate(R.layout.fragment_menu_jeans, container, false)
+    }
+
+    companion object {
+        private const val TAG = "MenuJeansFragment"
+    }
+}
