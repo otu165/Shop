ee986b3d4c8a5becb02268b8fb2fb45a833c2d1b
otu165 <micheu35@gmail.com>
2020-10-27 00:15:26 +0900

    juyeon_MyPageFragment_SignOut 구현

--- app/src/main/java/com/example/shopping/feature/login/MyPageFragment.kt
+++ app/src/main/java/com/example/shopping/feature/login/MyPageFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.shopping.R
+import com.example.shopping.api.FirebaseService
+import com.example.shopping.feature.main.MainActivity
+import com.google.firebase.ktx.Firebase
 
 class MyPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
@@ -22,6 +27,12 @@ class MyPageFragment : Fragment() {
     }
 
     private fun myPageFunction(view : View) {
+        val btnSignOut : Button = view.findViewById(R.id.btnFragMyPageSignOut)
 
+        btnSignOut.setOnClickListener {
+            FirebaseService.auth.signOut()
+            Toast.makeText(requireContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+            (activity as MainActivity).replaceFragment(SignInFragment())
+        }
     }
 }
